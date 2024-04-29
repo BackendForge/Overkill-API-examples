01030180bb9170dc562addc3c75eadda3272091c
Alpha Dragon <alphadragon@princeofcrypto.com>
2024-04-29 16:18:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Examples of Overkill API usage
+
+You can directly test the API using the examples, inside [attached **Jupyter notebook**](./endpoints_usage.ipynb).
+Remember to update API keys with yours.
+
+You can use attached `requirements.txt` to install required dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+There is also Python script, to access websocket channel.
+You can run it using the following command:
+
+```bash
+python3 websocket_client.py
+```
---
+++ websocket_client.py
@@ -0,0 +1,75 @@
+import asyncio
+import websockets
+import websockets.exceptions
+import os
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
+
+
+# Demo user (may not work in production)
+class DemoUser(BaseModel):
+    api_key: str = "aa1f2e2070e54928bdf8c11c18fdcee6"
+    api_secret: str = "6a1f3cdf59774234afce4c5ea038e5374df2499572f3470ca453acdaeec54748"
+
+
+# Creating a ThreadPoolExecutor for running input in a separate thread
+executor = ThreadPoolExecutor()
+
+
+async def get_input(prompt):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(executor, input, prompt)
+
+
+async def send_messages(websocket):
+    while True:
+        message = await get_input("")
+        try:
+            await websocket.send(message)
+            print("Message sent: ", message)
+        except websockets.exceptions.ConnectionClosedError:
+            print("Server is not available. Exiting...")
+            break
+
+
+async def receive_messages(websocket):
+    while True:
+        response = await websocket.recv()
+        print("Received: ", response)
+
+
+async def websocket_client():
+    API_KEY = os.getenv("API_KEY")
+    API_SECRET = os.getenv("API_SECRET")
+    uri = "wss://api.princeofcrypto.com/v1/stream/signals"
+    if API_KEY is None or API_SECRET is None:
+        logging.warning(
+            "API_KEY or API_SECRET not found in environment variables. Using demo user."
+        )
+        demo_user = DemoUser()
+        API_KEY = demo_user.api_key
+        API_SECRET = demo_user.api_secret
+    headers = {"x-api-key": API_KEY, "x-api-secret": API_SECRET}
+
+    async def async_websocket_connection(uri, headers):
+        async with websockets.connect(uri, extra_headers=headers) as websocket:
+            sender_task = asyncio.create_task(send_messages(websocket))
+            receiver_task = asyncio.create_task(receive_messages(websocket))
+            await asyncio.gather(sender_task, receiver_task)
+
+    # Connecting with headers
+    while True:
+        try:
+            await async_websocket_connection(uri, headers)
+        except websockets.exceptions.ConnectionClosedError:
+            print("Connection closed. Reconnecting in 5 sec...")
+            await asyncio.sleep(5)
+
+
+if __name__ == "__main__":
+    asyncio.run(websocket_client())
+# websockets.exceptions.ConnectionClosedError -> asyncio.exceptions.IncompleteReadError
